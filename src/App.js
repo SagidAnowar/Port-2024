468ddae406f77ffa0129d2667f09db0cfca0c375
Sagid Anowar <sagidanowar@gmail.com>
2024-01-16 19:23:17 -0500

    starting project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/about/About";
+import Footer from "./components/footer/Footer";
+import Hero from "./components/hero/Hero";
+import Nav from "./components/nav/Nav.";
+import Project from "./components/projects/Project";
+import Social from "./components/social/Social";
 
-function App() {
+function Portfolio() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <main>
+        <Hero />
+        <About />
+        <Project />
+        <Social />
+      </main>
+      <Footer />
+    </>
   );
 }
 
-export default App;
+export default Portfolio;
