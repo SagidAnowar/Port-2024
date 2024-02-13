8f916125849d0e4e8e3d47585c7fc0bf71b84214
Sagid Anowar <sagidanowar@gmail.com>
2024-02-13 14:32:35 -0500

    updating thank-you

--- src/pages/thank-you/Thank-you.jsx
+++ src/pages/thank-you/Thank-you.jsx
@@ -2,18 +2,20 @@ import UseAnimations from "react-useanimations";
 import mail from 'react-useanimations/lib/mail';
 import './thank-you.scss';
 import CustomButton from "../../components/custom-button/Custom-button";
-import { motion } from 'framer-motion';
+import { motion as Motion} from 'framer-motion';
 
 
 const ThankYou =() => (
-    <motion.div className="bounce-in-top">
+    <Motion.div className="bounce-in-top">
+
     <div className="animation">
         <UseAnimations  strokeColor="#33323D" className="animated" autoplay={true} speed={0.15} animation={mail} size={170} />
         <h1 className="thank-you">Thank You !</h1>
         <p className="thank-you-text">I will get back to you as soon as possible</p>
         <CustomButton  route={"/"}>HOME</CustomButton>
     </div>
-    </motion.div>
+    
+    </Motion.div>
 )
 
 export default ThankYou;
